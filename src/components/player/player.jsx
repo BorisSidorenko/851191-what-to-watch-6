5e64e0e53ba288d216d52dd3b358ab8a607c334d
Boris <boris.m.sidorenko@gmail.com>
2021-02-10 20:50:34 +0300

    добавил player компонент

---
+++ src/components/player/player.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import dayjs from 'dayjs';
+import duration from 'dayjs/plugin/duration';
+
+dayjs.extend(duration);
+
+const getHumanizeDuration = (durationInMinutes) => {
+  const parsedDuration = dayjs.duration(durationInMinutes, `minutes`);
+  return `${parsedDuration.$d.hours}:${parsedDuration.$d.minutes}`;
+};
+
+const Player = ({poster_image: poster, run_time: durationInMinutes}) => (
+  <div className="player">
+    <video src="#" className="player__video" poster={poster}></video>
+
+    <button type="button" className="player__exit">Exit</button>
+
+    <div className="player__controls">
+      <div className="player__controls-row">
+        <div className="player__time">
+          <progress className="player__progress" value="30" max="100"></progress>
+          <div className="player__toggler" style="left: 30%;">Toggler</div>
+        </div>
+        <div className="player__time-value">{getHumanizeDuration(durationInMinutes)}</div>
+      </div>
+
+      <div className="player__controls-row">
+        <button type="button" className="player__play">
+          <svg viewBox="0 0 19 19" width="19" height="19">
+            <use xlinkHref="#play-s"></use>
+          </svg>
+          <span>Play</span>
+        </button>
+        <div className="player__name">Transpotting</div>
+
+        <button type="button" className="player__full-screen">
+          <svg viewBox="0 0 27 27" width="27" height="27">
+            <use xlinkHref="#full-screen"></use>
+          </svg>
+          <span>Full screen</span>
+        </button>
+      </div>
+    </div>
+  </div>
+);
+
+Player.propTypes = {
+  "poster_image": PropTypes.string.isRequired,
+  "run_time": PropTypes.string.isRequired
+};
+
+export default Player;
