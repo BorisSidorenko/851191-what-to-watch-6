d9ded265a916c0e56bd8148f4848b42020ff7365
Boris <boris.m.sidorenko@gmail.com>
2021-02-11 21:16:22 +0300

    добавил роутинг в app

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,10 +1,32 @@
 import React from 'react';
 import Startup from '../startup/startup';
+import SignIn from '../sign-in/sign-in';
+import MyList from '../my-list/my-list';
+import MoviePage from '../movie-page/movie-page';
+import AddReview from '../add-review/add-review';
+import NotFound from '../not-found/not-found';
+import Player from '../player/player';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
 
 const App = (props) => (
-  <Startup
-    {...props}
-  />
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/">
+        <Startup {...props} />
+      </Route>
+      <Route exact path="/login">
+        <SignIn />
+      </Route>
+      <Route exact path="/mylist">
+        <MyList {...props} />
+      </Route>
+      <Route exact path="/films/:id" component={MoviePage}/>
+      <Route exact path="/films/:id/review" component={AddReview}/>
+      <Route>
+        <NotFound />
+      </Route>
+    </Switch>
+  </BrowserRouter>
 );
 
 export default App;
--- src/components/movie-page/movie-page.jsx
+++ src/components/movie-page/movie-page.jsx
@@ -3,78 +3,85 @@ import PropTypes from 'prop-types';
 import Header from '../header/header';
 import MovieCardDescription from '../movie-card-description/movie-card-description';
 import MovieCardButtons from '../movie-card-buttons/movie-card-buttons';
-import MovieCardDetails from '../movie-card-details/movie-card-details';
+import MovieCardOverview from '../movie-card-overview/movie-card-overview';
 import Catalog from '../catalog/catalog';
 import Footer from '../footer/footer';
 
-const MoviePage = ({movie, similarMovies}) => (
-  <>
-    <section className="movie-card movie-card--full">
-      <div className="movie-card__hero">
-        <div className="movie-card__bg">
-          <img src="img/bg-the-grand-budapest-hotel.jpg" alt="The Grand Budapest Hotel" />
-        </div>
+import {generateMovie, generateMovies} from '../../mock/movie/movie';
+import {AMOUNT_OF_SIMILAR_MOVIES} from '../../utils/constatns';
 
-        <h1 className="visually-hidden">WTW</h1>
+const MoviePage = ({match}) => {
+  const movie = generateMovie(match.params.id);
+  const {background_image: background, name} = movie;
 
-        <Header
-          headerClassName = "page-header movie-card__head"
-        />
-
-        <div className="movie-card__wrap">
-          <MovieCardDescription>
-            <MovieCardButtons>
-              <a href="add-review.html" className="btn movie-card__button">Add review</a>
-            </MovieCardButtons>
-          </MovieCardDescription>
-        </div>
-      </div>
-
-      <div className="movie-card__wrap movie-card__translate-top">
-        <div className="movie-card__info">
-          <div className="movie-card__poster movie-card__poster--big">
-            <img src="img/the-grand-budapest-hotel-poster.jpg" alt="The Grand Budapest Hotel poster" width="218" height="327" />
+  return (
+    <>
+      <section className="movie-card movie-card--full">
+        <div className="movie-card__hero">
+          <div className="movie-card__bg">
+            <img src={background} alt={name} />
           </div>
 
-          <div className="movie-card__desc">
-            <nav className="movie-nav movie-card__nav">
-              <ul className="movie-nav__list">
-                <li className="movie-nav__item movie-nav__item--active">
-                  <a href="#" className="movie-nav__link">Overview</a>
-                </li>
-                <li className="movie-nav__item">
-                  <a href="#" className="movie-nav__link">Details</a>
-                </li>
-                <li className="movie-nav__item">
-                  <a href="#" className="movie-nav__link">Reviews</a>
-                </li>
-              </ul>
-            </nav>
+          <h1 className="visually-hidden">WTW</h1>
 
-            <MovieCardDetails
+          <Header
+            headerClassName = "page-header movie-card__head"
+          />
+
+          <div className="movie-card__wrap">
+            <MovieCardDescription
               {...movie}
-            />
+            >
+              <MovieCardButtons>
+                <a href="add-review.html" className="btn movie-card__button">Add review</a>
+              </MovieCardButtons>
+            </MovieCardDescription>
           </div>
         </div>
+
+        <div className="movie-card__wrap movie-card__translate-top">
+          <div className="movie-card__info">
+            <div className="movie-card__poster movie-card__poster--big">
+              <img src="img/the-grand-budapest-hotel-poster.jpg" alt="The Grand Budapest Hotel poster" width="218" height="327" />
+            </div>
+
+            <div className="movie-card__desc">
+              <nav className="movie-nav movie-card__nav">
+                <ul className="movie-nav__list">
+                  <li className="movie-nav__item movie-nav__item--active">
+                    <a href="#" className="movie-nav__link">Overview</a>
+                  </li>
+                  <li className="movie-nav__item">
+                    <a href="#" className="movie-nav__link">Details</a>
+                  </li>
+                  <li className="movie-nav__item">
+                    <a href="#" className="movie-nav__link">Reviews</a>
+                  </li>
+                </ul>
+              </nav>
+
+              <MovieCardOverview
+                {...movie}
+              />
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <div className="page-content">
+        <Catalog
+          allMovies = {generateMovies(AMOUNT_OF_SIMILAR_MOVIES)}
+          showGenres = {false}
+          shwoButton = {false}
+          catalogClass = "catalog catalog--like-this"
+        >
+          <h2 className="catalog__title">More like this</h2>
+        </Catalog>
+
+        <Footer />
       </div>
-    </section>
-
-    <div className="page-content">
-      <Catalog
-        allMovies = {similarMovies}
-        catalogClass = "catalog catalog--like-this"
-      >
-        <h2 className="catalog__title">More like this</h2>
-      </Catalog>
-
-      <Footer />
-    </div>
-  </>
-);
-
-MoviePage.propTypes = {
-  movie: PropTypes.object.isRequired,
-  similarMovies: PropTypes.array.isRequired
+    </>
+  );
 };
 
 export default MoviePage;
