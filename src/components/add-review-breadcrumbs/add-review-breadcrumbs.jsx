2a84c6bd5ded29e1d155fc8fb2ef8cf0aaa67f8e
Boris <boris.m.sidorenko@gmail.com>
2021-02-12 21:57:25 +0300

    добавил Link в breadcrumbs

--- src/components/add-review-breadcrumbs/add-review-breadcrumbs.jsx
+++ src/components/add-review-breadcrumbs/add-review-breadcrumbs.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {Link} from 'react-router-dom';
 
 import {RoutePaths} from '../../utils/constatns';
 
@@ -7,7 +8,7 @@ const AddReviewBreadcrumbs = ({name, id}) => (
   <nav className="breadcrumbs">
     <ul className="breadcrumbs__list">
       <li className="breadcrumbs__item">
-        <a href={`${RoutePaths.MOVIE_PAGE}/${id}`} className="breadcrumbs__link">{name}</a>
+        <Link to={`${RoutePaths.MOVIE_PAGE}/${id}`} className="breadcrumbs__link">{name}</Link>
       </li>
       <li className="breadcrumbs__item">
         <a className="breadcrumbs__link">Add review</a>
