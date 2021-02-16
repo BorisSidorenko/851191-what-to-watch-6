b128538b74fbaa158970e97d95773ee4407d4daf
Boris <boris.m.sidorenko@gmail.com>
2021-02-16 13:39:24 +0300

    убрал функцию возвращающую компонент

--- src/components/movies-list/movies-list.jsx
+++ src/components/movies-list/movies-list.jsx
@@ -2,16 +2,9 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import MovieCard from '../movie-card/movie-card';
 
-const getMovieCardComponent = ({id, ...rest}) => (
-  <MovieCard
-    key={id}
-    {...rest}
-  />
-);
-
 const MoviesList = ({allMovies}) => (
   <div className="catalog__movies-list">
-    {allMovies.map(getMovieCardComponent)}
+    {allMovies.map(({id, ...rest}) => <MovieCard key={id} {...rest} />)}
   </div>
 );
 
