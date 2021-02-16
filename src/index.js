65390057a86aa9afee7cc57fb8f949d4ee7227ae
Boris <boris.m.sidorenko@gmail.com>
2021-02-16 13:30:28 +0300

    переименовал некоторые пропсы. передаю только необходимые пропсы

--- src/index.js
+++ src/index.js
@@ -2,14 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app';
 import Movies from '../src/mocks/movies';
+import Reviews from '../src/mocks/reviews';
 import {getRandomInt} from './utils/common';
 
 const movie = Movies[getRandomInt(Movies.length)];
 
 ReactDOM.render(
     <App
-      promoMovie={movie}
+      currentMovie={movie}
       allMovies={Movies}
+      reviews={Reviews}
     />,
     document.querySelector(`#root`)
 );
