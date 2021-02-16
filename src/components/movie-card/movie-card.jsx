b7b33950ec5df3b9035d8f96f82beb1f6b7ce256
Boris <boris.m.sidorenko@gmail.com>
2021-02-16 15:04:03 +0300

    добавил запись id фильма в стейт при ховере

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,20 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const MovieCard = ({name, preview_image: preview}) => (
-  <article className="small-movie-card catalog__movies-card">
-    <div className="small-movie-card__image">
-      <img src={preview} alt={name} width="280" height="175" />
-    </div>
-    <h3 className="small-movie-card__title">
-      <a className="small-movie-card__link" href="movie-page.html">{name}</a>
-    </h3>
-  </article>
-);
+const MovieCard = ({movieId, name, preview_image: preview, onMovieCardMouseEnter}) => {
+  const handleCardMouseEnter = () => {
+    onMovieCardMouseEnter(movieId);
+  };
+
+  return (
+    <article onMouseEnter={handleCardMouseEnter} className="small-movie-card catalog__movies-card">
+      <div className="small-movie-card__image">
+        <img src={preview} alt={name} width="280" height="175" />
+      </div>
+      <h3 className="small-movie-card__title">
+        <a className="small-movie-card__link" href="movie-page.html">{name}</a>
+      </h3>
+    </article>
+  );
+};
 
 MovieCard.propTypes = {
+  "movieId": PropTypes.number.isRequired,
   "preview_image": PropTypes.string.isRequired,
-  "name": PropTypes.string.isRequired
+  "name": PropTypes.string.isRequired,
+  "onMovieCardMouseEnter": PropTypes.func.isRequired
 };
 
 export default MovieCard;
