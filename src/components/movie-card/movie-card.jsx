286479505203c2f75f4acaf7d3f00f8682cb70f1
Boris <boris.m.sidorenko@gmail.com>
2021-02-25 14:59:18 +0300

    нашёл причину ошибки и устранил

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,11 +1,9 @@
 import React, {useState} from 'react';
 import {Link} from 'react-router-dom';
-import {connect} from 'react-redux';
 import PropTypes from 'prop-types';
 import VideoPlayer from '../video-player/video-player';
 import {RoutePaths} from '../../utils/constatns';
-import {idProp, previewProp, nameProp, genreProp} from '../props/movie-props';
-import {ActionCreator} from '../../store/action';
+import {idProp, previewProp, nameProp} from '../props/movie-props';
 
 const VIDEO_PREVIEW_DELAY = 1000;
 
@@ -17,7 +15,7 @@ const MovieCardTitle = ({movieId, name}) => (
   </h3>
 );
 
-const MovieCard = ({movieId, name, genre, preview_image: preview, onMovieCardMouseEnter, onMovieCardMouseLeave, currentMovieId, onMovieCardClick}) => {
+const MovieCard = ({movieId, name, preview_image: preview, onMovieCardMouseEnter, onMovieCardMouseLeave, currentMovieId}) => {
   const isNeedToPlay = movieId === currentMovieId;
   const [delayHandler, setDelayHandler] = useState(null);
 
@@ -33,17 +31,12 @@ const MovieCard = ({movieId, name, genre, preview_image: preview, onMovieCardMou
     onMovieCardMouseLeave();
   };
 
-  const handleMovieCardClick = () => {
-    const param = {
-      clickedMovieId: movieId,
-      similarGenre: genre
-    };
-
-    onMovieCardClick(param);
+  const handleCardClick = () => {
+    clearTimeout(delayHandler);
   };
 
   return (
-    <article onMouseEnter={handleCardMouseEnter} onMouseLeave={handleCardMouseLeave} onClick={handleMovieCardClick} className="small-movie-card catalog__movies-card">
+    <article onMouseEnter={handleCardMouseEnter} onMouseLeave={handleCardMouseLeave} onClick={handleCardClick} className="small-movie-card catalog__movies-card">
       <Link className="small-movie-card__link" to={`${RoutePaths.MOVIE_PAGE}/${movieId}`}>
         <div className="small-movie-card__image">
           {isNeedToPlay ? <VideoPlayer movieId={movieId} isPreview={true}/> : <MovieCardImage preview={preview} name={name}/>}
@@ -69,17 +62,8 @@ MovieCard.propTypes = {
   "currentMovieId": idProp,
   "preview_image": previewProp,
   "name": nameProp,
-  "genre": genreProp,
   "onMovieCardMouseEnter": PropTypes.func.isRequired,
-  "onMovieCardMouseLeave": PropTypes.func.isRequired,
-  "onMovieCardClick": PropTypes.func.isRequired
+  "onMovieCardMouseLeave": PropTypes.func.isRequired
 };
 
-const mapDispatchToProps = (dispatch) => ({
-  onMovieCardClick(param) {
-    dispatch(ActionCreator.loadSimilarMovies(param));
-  }
-});
-
-export {MovieCard};
-export default connect(null, mapDispatchToProps)(MovieCard);
+export default MovieCard;
