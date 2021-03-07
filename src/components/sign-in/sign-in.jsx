6dd59c5434485358a99e801532e251bbe64fbef7
Boris <boris.m.sidorenko@gmail.com>
2021-03-07 17:53:14 +0300

    переделал компоненты формы на контролируемые. добавил валидацию.

--- src/components/sign-in/sign-in.jsx
+++ src/components/sign-in/sign-in.jsx
@@ -1,19 +1,49 @@
-import React, {useRef} from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
 import {login} from '../../api/api-actions';
 import Header from '../header/header';
 import Footer from '../footer/footer';
 
-const SignIn = ({onSubmit}) => {
-  const emailRef = useRef();
-  const passwordRef = useRef();
+const EMAIL_ADDRESS_PATTERN = /^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/;
+const AMOUT_OF_PASSWORD_CHARACTERS = 6;
 
+const clearCustomValidity = (input) => {
+  input.setCustomValidity(``);
+  input.reportValidity();
+};
+
+const handleEmailChange = ({target}) => {
+  const isValueValid = EMAIL_ADDRESS_PATTERN.test(target.value);
+
+  if (!isValueValid) {
+    target.setCustomValidity(`The entered email is not valid. Please, enter email like example@mail.com`);
+    target.reportValidity();
+  } else {
+    clearCustomValidity(target);
+  }
+};
+
+const handlePasswordChange = ({target}) => {
+  const isPasswordValid = target.value.length >= AMOUT_OF_PASSWORD_CHARACTERS;
+
+  if (!isPasswordValid) {
+    target.setCustomValidity(`Your password should contain at least 6 characters.`);
+    target.reportValidity();
+  } else {
+    clearCustomValidity(target);
+  }
+};
+
+const SignIn = ({onSubmit}) => {
   const handleSubmitForm = (evt) => {
     evt.preventDefault();
+
+    const {email, password} = evt.target;
+
     onSubmit({
-      email: emailRef.current.value,
-      password: passwordRef.current.value
+      email: email.value,
+      password: password.value
     });
   };
 
@@ -27,11 +57,11 @@ const SignIn = ({onSubmit}) => {
         <form action="#" className="sign-in__form" onSubmit={handleSubmitForm}>
           <div className="sign-in__fields">
             <div className="sign-in__field">
-              <input className="sign-in__input" type="email" placeholder="Email address" name="user-email" id="user-email" ref={emailRef}/>
+              <input className="sign-in__input" type="email" placeholder="Email address" name="email" id="user-email" autoComplete="off" onChange={handleEmailChange}/>
               <label className="sign-in__label visually-hidden" htmlFor="user-email">Email address</label>
             </div>
             <div className="sign-in__field">
-              <input className="sign-in__input" type="password" placeholder="Password" name="user-password" id="user-password" autoComplete="off" ref={passwordRef} />
+              <input className="sign-in__input" type="password" placeholder="Password" name="password" id="user-password" autoComplete="off" onChange={handlePasswordChange}/>
               <label className="sign-in__label visually-hidden" htmlFor="user-password">Password</label>
             </div>
           </div>
