08908a98ec825e8644d2a0f34a8b21613e0d1477
Boris <boris.m.sidorenko@gmail.com>
2021-03-10 10:42:39 +0300

    вынес в app проверку на авторизацию

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,12 +1,46 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import PropTypes from 'prop-types';
 import LayoutRouter from '../layout-router/layout-router';
 import {Router} from 'react-router-dom';
 import browserHistory from '../../browser-history';
+import {checkAuth} from '../../api/api-actions';
+import {connect} from 'react-redux';
+import AuthCheck from '../auth-check/auth-check';
 
-const App = () => (
-  <Router history={browserHistory}>
-    <LayoutRouter />
-  </Router>
-);
+const App = ({isAuthtorized, onLoad}) => {
+  const [isChecking, setIsChecking] = useState(true);
 
-export default App;
+  useEffect(() => {
+    if (isChecking) {
+      onLoad();
+    }
+
+    return () => setIsChecking(false);
+  }, [isAuthtorized]);
+
+
+  if (!isAuthtorized && isChecking) {
+    return <AuthCheck />;
+  }
+
+  return (
+    <Router history={browserHistory}>
+      <LayoutRouter />
+    </Router>
+  );
+};
+
+App.propTypes = {
+  isAuthtorized: PropTypes.bool,
+  onLoad: PropTypes.func.isRequired
+};
+
+const mapStateToProps = ({isAuthtorized}) => ({isAuthtorized});
+
+const mapDispatchToProps = (dispatch) => ({
+  onLoad() {
+    dispatch(checkAuth());
+  }
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/components/private-route/private-route.jsx
+++ src/components/private-route/private-route.jsx
@@ -1,45 +1,24 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import {Route, Redirect} from 'react-router-dom';
 import {connect} from 'react-redux';
 import {RoutePaths} from '../../utils/constatns';
 import {ActionCreator} from '../../store/action';
-import {checkAuth} from '../../api/api-actions';
-import AuthCheck from '../auth-check/auth-check';
 
 const handleUserNotAuthtorized = ({location}, onPrivateRouteRequest) => {
   onPrivateRouteRequest(location.pathname);
   return <Redirect to={RoutePaths.SIGN_IN} />;
 };
 
-const PrivateRoute = ({render, isAuthtorized, onAuthCheck, onPrivateRouteRequest}) => {
-  const [isChecking, setIsChecking] = useState(true);
-
-  useEffect(() => {
-    if (isChecking) {
-      onAuthCheck();
-    }
-
-    return () => setIsChecking(false);
-  }, [isAuthtorized]);
-
-
-  if (!isAuthtorized && isChecking) {
-    return <AuthCheck />;
-  }
-
-  return (
-    <Route
-      render={(routeProps) => (
-        isAuthtorized ? render(routeProps) : handleUserNotAuthtorized(routeProps, onPrivateRouteRequest)
-      )}
-    />
-  );
-};
+const PrivateRoute = ({render, isAuthtorized, onPrivateRouteRequest}) => (
+  <Route
+    render={(routeProps) => (
+      isAuthtorized ? render(routeProps) : handleUserNotAuthtorized(routeProps, onPrivateRouteRequest)
+    )}
+  />
+);
 
 PrivateRoute.propTypes = {
-  path: PropTypes.string.isRequired,
-  onAuthCheck: PropTypes.func.isRequired,
   onPrivateRouteRequest: PropTypes.func.isRequired,
   render: PropTypes.func.isRequired,
   isAuthtorized: PropTypes.bool
@@ -48,9 +27,6 @@ PrivateRoute.propTypes = {
 const mapStateToProps = ({isAuthtorized}) => ({isAuthtorized});
 
 const mapDispatchToProps = (dispatch) => ({
-  onAuthCheck() {
-    dispatch(checkAuth());
-  },
   onPrivateRouteRequest(route) {
     dispatch(ActionCreator.addRequestedRoute(route));
   }
