7416a921b040a65a36736e22a6e8f28f0606864e
Boris <boris.m.sidorenko@gmail.com>
2021-03-16 18:10:52 +0300

    добавил редирект по клику на кнопку плей

--- src/components/movie-card-play-button/movie-card-play-button.jsx
+++ src/components/movie-card-play-button/movie-card-play-button.jsx
@@ -1,12 +1,32 @@
 import React from 'react';
+import {connect} from 'react-redux';
+import {useHistory} from 'react-router-dom';
+import {getSelectedMovie} from '../../store/data/selectors';
+import {RoutePaths} from '../../utils/constatns';
+import {idProp} from '../props/movie-props';
 
-const MovieCardPlayButton = () => (
-  <button className="btn btn--play movie-card__button" type="button">
-    <svg viewBox="0 0 19 19" width="19" height="19">
-      <use xlinkHref="#play-s"></use>
-    </svg>
-    <span>Play</span>
-  </button>
-);
+const MovieCardPlayButton = ({id}) => {
+  const history = useHistory();
+  const playerPath = `${RoutePaths.PLAYER}/${id}`;
 
-export default MovieCardPlayButton;
+  const handlePlayButtonClick = () => {
+    history.push(playerPath);
+  };
+
+  return (
+    <button className="btn btn--play movie-card__button" type="button" onClick={handlePlayButtonClick}>
+      <svg viewBox="0 0 19 19" width="19" height="19">
+        <use xlinkHref="#play-s"></use>
+      </svg>
+      <span>Play</span>
+    </button>
+  );
+};
+
+MovieCardPlayButton.propTypes = {
+  id: idProp
+};
+
+const mapStateToProps = (state) => ({id: getSelectedMovie(state).id});
+
+export default connect(mapStateToProps)(MovieCardPlayButton);
