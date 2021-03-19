6c312a12e8879314ef3bd3a9089f53f5a8d1ec28
Boris <boris.m.sidorenko@gmail.com>
2021-03-19 20:35:28 +0300

    поправил ошибку для приватных путей

--- src/components/private-route/private-route.jsx
+++ src/components/private-route/private-route.jsx
@@ -1,28 +1,30 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import PropTypes from 'prop-types';
 import {Route, Redirect} from 'react-router-dom';
 import {useSelector, useDispatch} from 'react-redux';
 import {RoutePaths} from '../../utils/constatns';
 import {addRequestedRouteAction} from '../../store/action';
 
-const handleUserNotAuthtorized = ({location}, onPrivateRouteRequest) => {
-  onPrivateRouteRequest(location.pathname);
+const handleUserNotAuthtorized = () => {
   return <Redirect to={RoutePaths.SIGN_IN} />;
 };
 
 const PrivateRoute = ({render, ...rest}) => {
+  const {location} = rest;
   const {isAuthtorized} = useSelector((state) => state.USER);
   const dispatch = useDispatch();
 
-  const onPrivateRouteRequest = (route) => {
-    dispatch(addRequestedRouteAction(route));
-  };
+  useEffect(() => {
+    if (!isAuthtorized) {
+      dispatch(addRequestedRouteAction(location.pathname));
+    }
+  }, []);
 
   return (
     <Route
       {...rest}
       render={(routeProps) => (
-        isAuthtorized ? render(routeProps) : handleUserNotAuthtorized(routeProps, onPrivateRouteRequest)
+        isAuthtorized ? render(routeProps) : handleUserNotAuthtorized()
       )}
     />
   );
