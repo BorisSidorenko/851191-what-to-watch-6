701471f77e1c4144b58331c020228497857e9f79
Boris <boris.m.sidorenko@gmail.com>
2021-03-30 20:42:42 +0300

    добавил редирект с логина на главную, если пользователь авторизован

--- src/components/sign-in/sign-in.test.js
+++ src/components/sign-in/sign-in.test.js
@@ -1,22 +1,53 @@
 import React from 'react';
+import {Route, Switch} from 'react-router-dom';
 import {render, screen} from '@testing-library/react';
 import {createMemoryHistory} from 'history';
-import {getStructureToRender} from '../../utils/test-utils';
+import {getStructureToRender, authInfoStructure} from '../../utils/test-utils';
+import {AuthorizationStatus, RoutePaths} from '../../utils/constatns';
+import configureStore from 'redux-mock-store';
 import userEvent from '@testing-library/user-event';
 import SignIn from './sign-in';
 
-it(`Should SignIn render correctly`, () => {
-  const history = createMemoryHistory();
+describe(`SingIn works correctly`, () => {
+  it(`Should render MAIN when user is authorized and navigates to '/login' url`, () => {
+    const history = createMemoryHistory();
+    const title = `Not SignIn Page`;
 
-  const structureToRender = getStructureToRender(history, <SignIn />);
-  render(structureToRender);
+    const structureToRender = getStructureToRender(
+        history,
+        <Switch>
+          <Route exact path={RoutePaths.MAIN}><h1>{title}</h1></Route>
+          <Route exact path={RoutePaths.SIGN_IN}>
+            <SignIn />
+          </Route>
+        </Switch>,
+    );
+    render(structureToRender);
 
-  expect(screen.getByLabelText(`Email address`)).toBeInTheDocument();
-  expect(screen.getByLabelText(`Password`)).toBeInTheDocument();
+    expect(screen.getByText(title)).toBeInTheDocument();
+  });
 
-  userEvent.type(screen.getByTestId(`email`), `keks`);
-  userEvent.type(screen.getByTestId(`password`), `123456`);
+  it(`Should render SignIn when user is not authorized`, () => {
+    const mockStore = configureStore({});
+    const store = mockStore({
+      USER: {
+        isAuthtorized: AuthorizationStatus.NOT_AUTHORIZED,
+        user: authInfoStructure
+      }
+    });
 
-  expect(screen.getByDisplayValue(`keks`)).toBeInTheDocument();
-  expect(screen.getByDisplayValue(`123456`)).toBeInTheDocument();
+    const history = createMemoryHistory();
+
+    const structureToRender = getStructureToRender(history, <SignIn />, store);
+    render(structureToRender);
+
+    expect(screen.getByLabelText(`Email address`)).toBeInTheDocument();
+    expect(screen.getByLabelText(`Password`)).toBeInTheDocument();
+
+    userEvent.type(screen.getByTestId(`email`), `keks`);
+    userEvent.type(screen.getByTestId(`password`), `123456`);
+
+    expect(screen.getByDisplayValue(`keks`)).toBeInTheDocument();
+    expect(screen.getByDisplayValue(`123456`)).toBeInTheDocument();
+  });
 });
