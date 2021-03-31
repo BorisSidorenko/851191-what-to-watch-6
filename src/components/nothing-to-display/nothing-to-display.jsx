44dbb2e19a1cda9d6cdde619b001e117aec647db
Boris <boris.m.sidorenko@gmail.com>
2021-03-31 13:14:08 +0300

    Updates

--- src/components/nothing-to-display/nothing-to-display.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const NothingToDisplay = () => (
-  <div style={{margin: `auto`}}>
-    <h3>No movies to display.</h3>
-  </div>
-);
-
-export default NothingToDisplay;
